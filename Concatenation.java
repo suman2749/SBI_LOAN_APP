8ea5ad333b9a9130707f79b241886fe685ae0529
Suman Maity <sumanmaity2749@gmail.com>
2024-10-15 13:05:11 +0530

    initial commit

---
+++ Concatenation.java
@@ -0,0 +1,10 @@
+public class Concatenation {
+    public static void main(String[] args) {
+        String firstName ="Suman";
+        String lastName ="Maity";
+        String fullName= firstName +" "+ lastName;
+        System.out.println(fullName);
+
+    }
+
+}
